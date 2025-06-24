828248fcc245691dde2bd3ed76adce003f40994a
oweitman <oweitman@gmx.de>
2025-06-24 16:31:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# ioBroker.pi-hole2
+
+![Logo](admin/pi-hole2.png)
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.pi-hole2.svg)](https://www.npmjs.com/package/iobroker.pi-hole2)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.pi-hole2.svg)](https://www.npmjs.com/package/iobroker.pi-hole2)
+![Number of Installations](https://iobroker.live/badges/pi-hole2-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/pi-hole2-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.pi-hole2.png?downloads=true)](https://nodei.co/npm/iobroker.pi-hole2/)
+
+**Tests:** ![Test and Release](https://github.com/oweitman/ioBroker.pi-hole2/workflows/Test%20and%20Release/badge.svg)
+
+## pi-hole2 adapter for ioBroker
+
+Manage a pi-hole installation >V6.
+Get information from pi-hole.
+Start/Stop blocking domains.
+(for pi-hole <V6 please use adapter ioBroker.pi-hole)
+
+USE AT YOUR OWN RISK!!! ABSOLUTELY NO WARRANTY FOR DAMAGES, ETC.!!!
+
+Help or hints are welcome.
+
+This adapter was rewritten for pi-hole V6 based on an idea by
+Michael Schuster <development@unltd-networx.de>.
+
+## Steps
+
+1. Install the adpater
+
+2. Fill in the fields of the adapter-admin. The url of the pi-hole device, the password, and obligatory the intervall to renew the values of the pi-hole (renew statistic in iobroker)
+
+## Functions
+
+### Enable/Disable the blocking
+
+To Enable/Disable the blocking, please use the switch in Datapoint Blocking. The BlockingTime is only used for disabling the blocking to automaticly reenable the blocking. Enabling takes place immediately.
+
+## Todo Existing Functions
+
+- ~~login~~
+- ~~interval time~~
+- ~~activate / decativate blocking~~
+- ~~activating / deactivating timeinterval~~
+- version ? dont know details
+- ~~versions~~
+- type
+- summaryRaw ? dont know details
+- ~~summary~~
+- topItems ? dont know details
+- getQuerySources ? dont know details
+- overTimeData10mins ? dont know details
+- getForwardDestinations ? dont know details
+
+## Todo New Functions
+
+- sendTo Functions to control and get informations with parameters
+
+## Changelog
+
+<!--
+    Placeholder for the next version (at the beginning of the line):
+    ### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+
+- (oweitman) initial release
+
+## License
+
+MIT License
+
+Copyright (c) 2025 oweitman <oweitman@gmx.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,128 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v2.6.5
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+const piholeclassNew = require(`${__dirname}/lib/piholeserver.js`);
+let piholeserver;
+
+class PiHole2 extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'pi-hole2',
+        });
+        this.pihole = null;
+        this.on('ready', this.onReady.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        // this.on('objectChange', this.onObjectChange.bind(this));
+        // this.on('message', this.onMessage.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Initialize your adapter here
+        if (!piholeserver) {
+            this.log.debug('main onReady open pihole');
+            piholeserver = new piholeclassNew(this);
+            await piholeserver.init();
+        }
+
+        // Reset the connection indicator during startup
+        this.setState('info.connection', false, true);
+
+        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+        this.subscribeStates('*');
+    }
+    requestData() {
+
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            // Here you must clear all timeouts or intervals that may still be active
+            // clearTimeout(timeout1);
+            // clearTimeout(timeout2);
+            // ...
+            // clearInterval(interval1);
+
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
+    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
+    // /**
+    //  * Is called if a subscribed object changes
+    //  * @param {string} id
+    //  * @param {ioBroker.Object | null | undefined} obj
+    //  */
+    // onObjectChange(id, obj) {
+    //     if (obj) {
+    //         // The object was changed
+    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+    //     } else {
+    //         // The object was deleted
+    //         this.log.info(`object ${id} deleted`);
+    //     }
+    // }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            if (piholeserver) {
+                piholeserver.stateChange(id, state);
+            }
+        }
+    }
+
+    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
+    // /**
+    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
+    //  * @param {ioBroker.Message} obj
+    //  */
+    // onMessage(obj) {
+    //     if (typeof obj === 'object' && obj.message) {
+    //         if (obj.command === 'send') {
+    //             // e.g. send email or pushover or whatever
+    //             this.log.info('send command');
+
+    //             // Send response in callback if required
+    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+    //         }
+    //     }
+    // }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new PiHole2(options);
+} else {
+    // otherwise start the instance directly
+    new PiHole2();
+}
